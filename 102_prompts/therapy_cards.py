1af36b5e677ff9a01ebaa1cbbe779ff75cabb8f7
andrea-sweny <andrea.sweny@validusresearch.com>
2024-03-11 13:28:24 -0400

    march work

--- 102_prompts/therapy_cards.py
+++ 102_prompts/therapy_cards.py
@@ -1,18 +1,4 @@
 prompts_data = {
-    "Book Quotations": [
-        {"title": "How to Transform Your Life",
-        "text": """Even if we fulfill all the wishes of our self-cherishing, there is no guarantee that we would be 
-        happy, for every attainment brings with it new problems and invariably leads to new desires. The relentless
-        pursuit of our selfish desires is like drinking salt water to quench our thirst: the more we indulge our 
-        desires, the greater our thirst."""},
-        {
-         "title": "How to Transform Your Life",
-            "text": """It is because of self-cherishing that we take our wishes and plans so seriously and are unable 
-            to accept and learn from the difficulties life brings us. We do not become a better person just by 
-            fulfilling our wishes for worldly success; we are as likely to develop the qualities that really matter - 
-            wisdom, patience, compassion - through our failures as through our successes."""
-        },
-    ],
     "Animal Spirit Guides": [
         {
             "title": "Panda",
